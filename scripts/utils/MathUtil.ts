f582ee187d3d3137129d7eef1be5f53185c12650
Цолак Асланян <a.tsolak@gmail.com>
2025-07-28 23:43:14 +0300

    Добавлены несколько методов в утилиты

--- scripts/utils/MathUtil.ts
+++ scripts/utils/MathUtil.ts
@@ -1,4 +1,4 @@
-import { _decorator } from 'cc';
+import { _decorator, Vec2, Vec3, view } from 'cc';
 const { ccclass } = _decorator;
 
 @ccclass('MathUtil')
@@ -63,5 +63,80 @@ export class MathUtil
 
         // Возвращаем true, если случайное число меньше заданной вероятности
         return randomValue < probabilityPercent;
+    }
+
+    /**
+     * Применяет ScaleX и ScaleY игры к переданному вектору
+     * @param vec3 
+     */
+    public static applyGameScaleToVec3(vec3:Vec3):void
+    {
+        vec3.x /= view.getScaleX();
+        vec3.y /= view.getScaleY();
+    }
+
+    /**
+     * Применяет ScaleX и ScaleY игры к переданному вектору
+     * @param vec2
+     */
+    public static applyGameScaleToVec2(vec2:Vec2):void
+    {
+        vec2.x /= view.getScaleX();
+        vec2.y /= view.getScaleY();
+    }
+
+    /**
+     * Клонирует вектор и возвращает его с применённым ScaleX и ScaleY игры
+     * @param vec3 
+     */
+    public static getGameScaledVec3(vec3:Vec3):Vec3
+    {
+        let newVec3:Vec3 = vec3.clone();
+        newVec3.x /= view.getScaleX();
+        newVec3.y /= view.getScaleY();
+
+        return newVec3;
+    }
+
+    /**
+     * Клонирует вектор и возвращает его с применённым ScaleX и ScaleY игры
+     * @param vec2
+     */
+    public static getGameScaledVec2(vec2:Vec2):Vec2
+    {
+        let newVec2:Vec2 = vec2.clone();
+        newVec2.x /= view.getScaleX();
+        newVec2.y /= view.getScaleY();
+
+        return newVec2;
+    }
+
+    /**
+     * Метод округляет в меньшую сторону до указанного разряда
+     * @param num число которое нужно округлить
+     * @param precision точность округления (разряд) 10-тые, 100-сотыеб 1000-тысячные
+     * @returns 
+     */
+    public static roundDownTo(num:number, precision:number):number
+    {
+        return Math.floor(num / precision) * precision;
+    }
+
+    /**
+     * Метод возвращает массив с указанным диапозоном чисел
+     * @param start начало диапозона (включительно)
+     * @param end конец диапозона (включительно)
+     * @returns массив чисел [min, min + 1, ..., max]
+     */
+    public static rangeArray(start:number, end:number):number[]
+    {
+        let result:number[] = [];
+
+        for (let i = start; i <= end; i++)
+        {
+            result.push(i);
+        }
+
+        return result;
     }
 }
