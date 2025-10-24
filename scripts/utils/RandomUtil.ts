a34de0f7d1c2d1db87f613cb7064d182572d12fb
Цолак Асланян <a.tsolak@gmail.com>
2025-10-24 19:00:10 +0300

    Добавил рандомных методов

--- scripts/utils/RandomUtil.ts
+++ scripts/utils/RandomUtil.ts
@@ -1,4 +1,4 @@
-import { _decorator, randomRangeInt } from 'cc';
+import { _decorator, Color, randomRangeInt } from 'cc';
 const { ccclass } = _decorator;
 
 @ccclass('RandomUtil')
@@ -27,5 +27,56 @@ export class RandomUtil
     public static randArrayValue<T>(arrayObj:T[]):T
     {
         return arrayObj[randomRangeInt(0, arrayObj.length)];
+    }
+
+    /**
+     * Метод возвращает не прозрачный случайный цвет
+     * 
+     * @returns 
+     */
+    public static randomColor():Color
+    {
+        return new Color(
+                    Math.floor(Math.random() * 255),
+                    Math.floor(Math.random() * 255),
+                    Math.floor(Math.random() * 255),
+                    255
+                );
+    }
+
+    /**
+     * Алгоритм Фишера-Йетса (современная версия) перемешивает массив обеспечивает равномерное распределение.
+     * Не изменяет оригинальный массив.
+     * 
+     * @param array 
+     * @returns возвращается новый массив оригинальный не изменяется
+     */
+    public static shuffleArrayImmutable<T>(array:T[]):T[]
+    {
+        const shuffled = [...array];
+
+        for (let i = shuffled.length - 1; i > 0; i--)
+        {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+
+        return shuffled;
+    }
+
+    /**
+     * Алгоритм Фишера-Йетса (современная версия) перемешивает массив обеспечивает равномерное распределение.
+     * Изменяет оригинальный массив.
+     * 
+     * @param array 
+     * @returns
+     */
+    public static shuffleArrayMutable<T>(array:T[]):void
+    {
+        for (let i = array.length - 1; i > 0; i--)
+        {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
     }
 }
